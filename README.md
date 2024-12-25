50d8e366534d038b191272e3d8beb4254495ceac
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-12-25 07:19:45 +0000

    - r refactor standard deviation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![Run Tests](https://github.com/ab492/FinanceTools/actions/workflows/run_tests.yml/badge.svg)](https://github.com/ab492/FinanceTools/actions/workflows/run_tests.yml)
 
 TODO
+- Variance.
 - Look into overflow issue with large numbers.
 - Look into precision in Python.
--- utilities.py
+++ utilities.py
@@ -6,19 +6,15 @@ def calculate_standard_deviation(data):
     :param data: List of numerical values.
     :return: Standard deviation of the dataset.
     """
-    if len(data) == 1:
-        return 0
-    if all(x == data[0] for x in data):
-        return 0
-    else:
-        all_squared_distances_from_mean = []
-        mean = calculate_mean(data)
-        for x in data:
-            distance_from_mean = x - mean
-            all_squared_distances_from_mean.append(distance_from_mean * distance_from_mean)
-        
-        variance = calculate_mean(all_squared_distances_from_mean)
-        return math.sqrt(variance)
+    all_squared_distances_from_mean = []
+    mean = calculate_mean(data)
+    for x in data:
+        distance_from_mean = x - mean
+        distance_from_mean_squared = distance_from_mean * distance_from_mean
+        all_squared_distances_from_mean.append(distance_from_mean_squared)
+    
+    variance = calculate_mean(all_squared_distances_from_mean)
+    return math.sqrt(variance)
 
 def calculate_mean(data):
     return sum(data) / len(data)
